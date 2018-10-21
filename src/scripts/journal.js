19e05b7b41b03d5d20378d911bc2e7f864aa0786
robbyhecht <robbyhecht@gmail.com>
2018-10-20 19:55:35 -0500

    added journal.js with instructions, added article tag to index.html

---
+++ src/scripts/journal.js
@@ -0,0 +1,34 @@
+
+const journalEntries = [
+  {
+      concept: "Array methods",
+      date: "07/24/2018",
+      entry: "We learned about array methods, but only forEach made sense",
+      mood: "Ok"
+  }
+]
+
+/*
+  Purpose: To create, and return, a string template that
+  represents a single journal entry object as HTML
+
+  Arguments: journalEntry (object)
+*/
+const makeJournalEntryComponent = (journalEntry) => {
+  // Create your own HTML structure for a journal entry
+  return `
+  
+  `
+}
+
+/*
+    Purpose: To render all journal entries to the DOM
+
+    Arguments: entries (array of objects)
+*/
+const renderJournalEntries = (entries) => {
+
+}
+
+// Invoke the render function
+renderJournalEntries(journalEntries)
