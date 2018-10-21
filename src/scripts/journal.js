5377d4d6272043a4f0f3c020c4f7a8954ca3935c
robbyhecht <robbyhecht@gmail.com>
2018-10-21 14:01:52 -0500

    Made journal work with existing array data. Still need to figure out how to feed in the data from the user entries

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,54 +1,71 @@
 
-// CONSTRUCTION ZONE... WORK IN PROGRESS 💀
+// -- Still need to make work with input data --
 
-// empty entries array
-const journalEntries = []
-// make added article tag into variable for dom placement
-let entryLog = document.getElementsByClassName("entryLog");
-// make inputs into variables
-  let journalDate = document.getElementById("journalDate")
-  let journalConcept = document.getElementById("journalConcept")
-  let journalMood = document.getElementById("journalMood")
-  let journalEntry = document.getElementById("journalEntry")
-
-  // and create a new object with them
-  let recentEntry = {
-    concept: journalConcept.value,
-    entry: journalEntry.value,
-    mood: journalMood.value,
-    date: journalDate.value
+// Array of existing journal entries
+const journalEntries = [
+  {
+    date: "10/21/2018",
+    concept: "Functions",
+    entry: "Freakin functions. So functional!",
+    mood: "getting there"
+  },
+  {
+      date: "10/20/2018",
+      concept: "Arrays",
+      entry: "We learned about different array methods today. forEach made sense FINALLY",
+      mood: "killer"
   }
-  journalEntries.unshift(recentEntry)
+];
 
-console.log(journalEntries)
+// Make a variable for DOM placement from the empty article tag in index.html -- this is where the journal entries will appear in the browser.
+const entryLog = document.querySelector(".entryLog");
+// Q: why doesn't this work with getElementsByClassName?
 
-// ??
 
-let makeJournalEntry = (journalEntries) => {
-  for (i=0; i<journalEntries.length; i++) {
-    let collection = `
-    <h3>${journalEntries[i].concept}</h3>
-    <p>${journalEntries[i].entry}</p>
-    <p>${journalEntries[i].mood}</p>
-    <p>${journalEntries[i].date}</p>
-  `
-  }
-  return collection;
+// This function reformats the array's objects into user-friendly html for display in the DOM
+let createJournalEntry = (entryObject) => {
+  let htmlContent = `
+    <section class="userEntry"> 
+      <h2>${entryObject.concept}</h2>
+      <p>${entryObject.entry}</p>
+      <p><strong>Feelings check:</strong> ${entryObject.mood}</p>
+      <p>${entryObject.date}</p> 
+    </section>
+    <hr>`
+    return htmlContent;
 }
 
-let entryObject = document.createElement("div");
-let insert = () => entryObject.appendChild(collection);
+// For each object in the array (aka each journal entry), transform the entry into html with the createJournalEntry function above and add it to the DOM's entryLog article
+addJournalEntry = (entries) => {
+  entries.forEach(entry => {
+    entryLog.innerHTML += createJournalEntry(entry);
+  });
+}
+
+// Call the function to activate the journal!
+addJournalEntry(journalEntries);
 
 
 
-// /*
-//     Purpose: To render all journal entries to the DOM
+// // make inputs into variables
+//   let journalDate = document.querySelector("#journalDate")
+//   let journalConcept = document.querySelector("#journalConcept")
+//   let journalMood = document.querySelector("#journalMood")
+//   let journalEntry = document.querySelector("#journalEntry")
 
-//     Arguments: entries (array of objects)
-// */
-// const renderJournalEntries = (entries) => {
+//   // and create a new object with them
+//   let recentEntry = {
+//     concept: journalConcept.value,
+//     entry: journalEntry.value,
+//     mood: journalMood.value,
+//     date: journalDate.value
+//   }
+//   journalEntries.unshift(recentEntry)
 
-// }
+// console.log(journalEntries)
 
-// // Invoke the render function
-// renderJournalEntries(journalEntries)
+// // ??
+
+// let entryObject = document.createElement("div");
+// let insert = () => entryObject.appendChild(collection);
+
