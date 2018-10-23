cf6d3ff8727d00c69ebd0e054e707a550d79f794
robbyhecht <robbyhecht@gmail.com>
2018-10-23 09:53:50 -0500

    made single responsibility modules for step 1 of journal assignment 4

---
+++ src/scripts/data.js
@@ -0,0 +1,9 @@
+// Fetch the array from the API and run it through the functions above to post objects to the dom
+
+fetch("http://localhost:8088/entries") // Fetch from the API
+    .then(entry => entry.json())  // Parse as JSON
+    .then(entries => {
+      createJournalEntry(entries)
+        // What should happen when we finally have the array?
+      addJournalEntry(entries)
+    })
---
+++ src/scripts/entriesDOM.js
@@ -0,0 +1,10 @@
+// Make a variable for DOM placement from the empty article tag in index.html
+// Q: why doesn't this work with getElementsByClassName?
+const entryLog = document.querySelector(".entryLog");
+
+// For each object in the array (aka each journal entry), transform the entry into html with the createJournalEntry function above and add it to the DOM's entryLog article
+addJournalEntry = (entries) => {
+  entries.forEach(entry => {
+    entryLog.innerHTML += createJournalEntry(entry);
+  });
+}
