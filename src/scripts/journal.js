d80adb84bfae99ffef996966e368a25d43fdeb1f
robbyhecht <robbyhecht@gmail.com>
2018-10-29 15:00:50 -0500

    added data module to handle api fetch per instructions. made entriesDOM module more dry. populated main journal js file to run the processes from a central location.

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,1 +1,22 @@
 
+// Call the API object's function
+API.getJournalEntries()
+// Run the entries through the existing create and add functions
+.then(entries =>
+  // Creates and adds html to DOM
+  addJournalEntry(entries)
+)
+
+
+
+
+
+/*
+    Main application logic that uses the functions and objects
+    defined in the other JavaScript files.
+
+    Change the fake variable names below to what they should be
+    to get the data and display it.
+*/
+
+// objectWithGetterMethod.methodToGetData().then(functionThatRendersData)
