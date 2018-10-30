edde38fe6bdd962c8ca5080a809a2dbbac9b1cb8
robbyhecht <robbyhecht@gmail.com>
2018-10-29 21:11:36 -0500

    finalized journal 4

--- src/scripts/entryComponent.js
+++ src/scripts/entryComponent.js
@@ -2,7 +2,7 @@
 const domManager = {
 createJournalEntry (entryObject) {
   let htmlContent = `
-    <section class="userEntry"> 
+    <section class="userEntry" class="visibleWords"> 
       <h2>${entryObject.concept}</h2>
       <p>${entryObject.entry}</p>
       <p><strong>Feelings check:</strong> ${entryObject.mood}</p>
@@ -12,19 +12,6 @@ createJournalEntry (entryObject) {
     return htmlContent;
   }
 }
-
-
-
-
-// Change the code in both src/scripts/entriesDOM.js and src/scripts/entryComponent.js so that the functions in each one becomes a method on an object, just like the code for API does above. Use Object.create.
-
-// When you are done, there should be three objects defined in your application.
-
-// One object that has a method for API access
-// One object that has a method for building a component
-// One object that has a method rendering the components to the DOM
-// Refactor: Once the objects are defined, refactor your code to use the methods on those objects where needed.
-
 
 // old function composition:
 
--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -5,19 +5,3 @@ API.getJournalEntries()
 .then(entries =>
   renderDom.addJournalEntry(entries)
 )
-
-
-
-
-
-
-
-
-// Change the code in both src/scripts/entriesDOM.js and src/scripts/entryComponent.js so that the functions in each one becomes a method on an object, just like the code for API does above. Use Object.create.
-
-// When you are done, there should be three objects defined in your application.
-
-// One object that has a method for API access
-// One object that has a method for building a component
-// One object that has a method rendering the components to the DOM
-// Refactor: Once the objects are defined, refactor your code to use the methods on those objects where needed.
